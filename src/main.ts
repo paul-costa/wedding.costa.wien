623c8a1f60c256637a698cb30bc8c87ed3b05838
Paul Costa <costa-paul@outlook.com>
2024-07-14 15:33:28 +0200

    navbar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './modules/app.module';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { firebaseConfig } from './assets/firebase.config';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
