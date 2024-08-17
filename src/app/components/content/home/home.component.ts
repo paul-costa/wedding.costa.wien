236496a4e0de1f756d70b6ad13d696210c638e7b
Paul Costam <costa-paul@outlook.com>
2024-08-17 23:03:48 +0200

    basic firestore implementation

--- src/app/components/content/home/home.component.ts
+++ src/app/components/content/home/home.component.ts
@@ -1,19 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
+import { Homepage } from 'src/app/constants/fire-store.types';
 import { FireStoreService } from 'src/app/services/fire-store.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
+  homePageData?: Homepage[];
+
   private readonly fireStoreService = inject(FireStoreService);
 
   ngOnInit() {
     this.fireStoreService.getHomePageData().then((data) => {
-      console.log(data);
+      this.homePageData = data;
     });
   }
 }
