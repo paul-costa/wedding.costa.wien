b26e21342bf4693dd526eee54ac0541f8c3089f2
Paul Costam <costa-paul@outlook.com>
2025-02-23 00:16:51 +0100

    messages in seperate component

--- src/app/components/content/home/home.component.ts
+++ src/app/components/content/home/home.component.ts
@@ -1,84 +1,21 @@
-import { CommonModule, NgIf } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { from, Observable } from 'rxjs';
-import { Homepage, Message } from 'src/app/constants/fire-store.types';
-import { HomePageContent, LoadingState } from 'src/app/constants/shared-interfaces';
+import { Homepage } from 'src/app/constants/fire-store.types';
 import { FireStoreService } from 'src/app/services/fire-store.service';
-
-const materialModules = [MatInputModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule];
+import { MessagesComponent } from './messages/messages.component';
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, ReactiveFormsModule, NgIf, ...materialModules],
+  imports: [AsyncPipe, MessagesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  currentMessage = new FormControl<string>('', [Validators.required]);
-
-  currentMessagesLoadingState = LoadingState.None;
-  messages: Message[] = [];
-
   readonly homePageData$: Observable<Homepage[]>;
-  readonly LoadingState = LoadingState;
-  readonly content: HomePageContent = {
-    messagesHeaderLabel: 'Nachrichten',
-    messagesLoadingErrorLabel: 'Nachrichten konnten nicht geladen werden!',
-    messagesInputHeaderLabel: 'Nachricht',
-    messagesInputInvalidLabel: 'Nachricht ist ein Pflichtfeld',
-  };
 
   private readonly fireStoreService = inject(FireStoreService);
 
   constructor() {
     this.homePageData$ = from(this.fireStoreService.getHomePageData());
-
-    this.fetchAndSetMessages();
-  }
-
-  onSendMessageButtonClick() {
-    this.currentMessage.markAsTouched();
-
-    if (this.currentMessage.invalid) {
-      return;
-    }
-
-    this.currentMessagesLoadingState = LoadingState.Loading;
-
-    const message: Message = {
-      id: crypto.randomUUID(),
-      value: this.currentMessage.value,
-      date: new Date(),
-    };
-
-    this.fireStoreService.sendMessage(message).then((res) => {
-      if (res) {
-        this.currentMessage.reset();
-        this.fetchAndSetMessages();
-        return;
-      }
-
-      this.currentMessagesLoadingState = LoadingState.Error;
-    });
-  }
-
-  private fetchAndSetMessages() {
-    this.currentMessagesLoadingState = LoadingState.Loading;
-
-    this.fireStoreService
-      .getMessages()
-      .catch((err) => {
-        this.currentMessagesLoadingState = LoadingState.Error;
-        console.error(err);
-        return [] as Message[];
-      })
-      .then((res) => {
-        this.messages = res;
-        this.currentMessagesLoadingState = LoadingState.Success;
-      });
   }
 }
---
+++ src/app/components/content/home/messages/messages.component.ts
@@ -0,0 +1,81 @@
+import { DatePipe, NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Message } from 'src/app/constants/fire-store.types';
+import { HomePageContent, LoadingState } from 'src/app/constants/shared-interfaces';
+import { FireStoreService } from 'src/app/services/fire-store.service';
+
+const materialModules = [MatInputModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule];
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.scss',
+  imports: [NgClass, ReactiveFormsModule, DatePipe, ...materialModules],
+})
+export class MessagesComponent {
+  currentMessage = new FormControl<string>('', [Validators.required]);
+
+  currentMessagesLoadingState = LoadingState.None;
+  messages: Message[] = [];
+  readonly LoadingState = LoadingState;
+  readonly content: HomePageContent = {
+    messagesHeaderLabel: 'Nachrichten',
+    messagesLoadingErrorLabel: 'Nachrichten konnten nicht geladen werden!',
+    messagesInputHeaderLabel: 'Nachricht',
+    messagesInputInvalidLabel: 'Nachricht ist ein Pflichtfeld',
+  };
+
+  private readonly fireStoreService = inject(FireStoreService);
+
+  constructor() {
+    this.fetchAndSetMessages();
+  }
+
+  onSendMessageButtonClick() {
+    this.currentMessage.markAsTouched();
+
+    if (this.currentMessage.invalid) {
+      return;
+    }
+
+    this.currentMessagesLoadingState = LoadingState.Loading;
+
+    const message: Message = {
+      id: crypto.randomUUID(),
+      value: this.currentMessage.value,
+      date: new Date(),
+    };
+
+    this.fireStoreService.sendMessage(message).then((res) => {
+      if (res) {
+        this.currentMessage.reset();
+        this.fetchAndSetMessages();
+        return;
+      }
+
+      this.currentMessagesLoadingState = LoadingState.Error;
+    });
+  }
+
+  private fetchAndSetMessages() {
+    this.currentMessagesLoadingState = LoadingState.Loading;
+
+    this.fireStoreService
+      .getMessages()
+      .catch((err) => {
+        this.currentMessagesLoadingState = LoadingState.Error;
+        console.error(err);
+        return [] as Message[];
+      })
+      .then((res) => {
+        this.messages = res;
+        this.currentMessagesLoadingState = LoadingState.Success;
+      });
+  }
+}
