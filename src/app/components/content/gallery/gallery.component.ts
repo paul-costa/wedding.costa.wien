12ddc305e9aea7fa4f51293b30e96ba6c523fa7f
Paul Costam <costa-paul@outlook.com>
2025-03-31 05:08:16 +0200

    add location

--- src/app/components/content/gallery/gallery.component.ts
+++ src/app/components/content/gallery/gallery.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-gallery',
-    imports: [],
-    templateUrl: './gallery.component.html',
-    styleUrl: './gallery.component.scss'
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.scss',
 })
-export class GalleryComponent {
-
-}
+export class GalleryComponent {}
--- src/modules/app-routing.module.ts
+++ src/modules/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ContentComponents } from '../app/constants/app.constants';
+import { provideRouter, RouterModule, Routes, withComponentInputBinding } from '@angular/router';
+import { DosDontsComponent } from 'src/app/components/content/dos-donts/dos-donts.component';
+import { DresscodeComponent } from 'src/app/components/content/dresscode/dresscode.component';
+import { GalleryComponent } from 'src/app/components/content/gallery/gallery.component';
+import { GiftsComponent } from 'src/app/components/content/gifts/gifts.component';
+import { HomeComponent } from 'src/app/components/content/home/home.component';
+import { LocationComponent } from 'src/app/components/content/location/location.component';
+import { TimetableComponent } from 'src/app/components/content/timetable/timetable.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'location', component: LocationComponent },
+  { path: 'timetable', component: TimetableComponent },
+  { path: 'dresscode', component: DresscodeComponent },
+  { path: 'dos-donts', component: DosDontsComponent },
+  { path: 'gifts', component: GiftsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  {
+    path: '*',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [provideRouter(routes, withComponentInputBinding()), Location, { provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
-export class AppRoutingModule {
-  constructor() {
-    for (let contentComponent of Object.values(ContentComponents)) {
-      routes.push({
-        path: contentComponent.url,
-        component: contentComponent.component,
-      });
-    }
-  }
-}
+export class AppRoutingModule {}
